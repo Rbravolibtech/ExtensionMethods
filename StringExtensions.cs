33bccff31d81a0c291e93f78460ef765046b2ab5
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2024-01-08 15:29:50 -0700

    added code for string extensions and moved to different file

---
+++ StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace ExtensionMethods // Removed semicolon from here
+{
+    public static class StringExtensions
+    {
+        public static string Shorten(this String str, int numberOfWords)
+        {
+
+        }
+    }
+}
