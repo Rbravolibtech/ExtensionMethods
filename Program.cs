fbe946dd857d3d7d326c6f9320f538134ffa274a
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2024-01-08 15:41:57 -0700

    coding how to do extension methods

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+namespace ExtensionMethods 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is a Post about Extension Methods";
+            var shortendedPost = post.Shorten(5);
 
+            Console.WriteLine(shortendedPost);
+        }
+        
+    }
+}
--- StringExtensions.cs
+++ StringExtensions.cs
@@ -4,7 +4,20 @@
     {
         public static string Shorten(this String str, int numberOfWords)
         {
+            if (numberOfWords < 0)
+                throw new ArgumentOutOfRangeException("numberOfWords should be greater than or equal to 0");
+            if (numberOfWords == 0)
+                return "";
 
+            var words = str.Split(' ');
+
+            if (words.Length <= numberOfWords)
+                return str;
+
+            //the +"..."; on the code below makes it so what you consolelog wont all show but it willl
+            //nofity you that you have more words
+
+        return string.Join(" ",words.Take(numberOfWords)) + "...";
         }
     }
 }
